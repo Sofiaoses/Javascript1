2daed2f94791749409e2fc029d60818aa2969c10
Sofiaoses <100168402+Sofiaoses@users.noreply.github.com>
2022-05-11 09:15:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 let productos = [
     {
         nombre: "Lechuga",
-        precio: 30,
+        categoria: "Verdura",
+        procedencia: "Argentina",
+        precio: 300,
+        disponibilidad: true,
+        maduracion: "Madura"
     },
     {
         nombre: "Manzana",
+        categoria: "Fruta",
+        procedencia: "Bolivia",
         precio: 150,
+        disponibilidad: true,
+        maduracion: "Madura"
     },
     {
         nombre: "Banana",
+        categoria: "Fruta",
+        procedencia: "Chile",
         precio: 200,
+        disponibilidad: false,
+        maduracion: "No madura",
     },
     {
         nombre: "Cebolla",
+        categoria: "Verdura",
+        procedencia: "Argentina",
         precio: 78,
+        disponibilidad: true,
+        maduracion: "Madura",
     },
     {
         nombre: "Tomate",
-        precio: 100,
+        categoria: "Verdura",
+        procedencia: "Argentina",
+        precio: 500,
+        disponibilidad: true,
+        maduracion: "Madura"
     },
 ];
 
 
-localStorage.clear();
+//localStorage.clear();
 
 
 function agregarALocalStorage(producto) {
@@ -50,19 +70,39 @@ function agregarALocalStorage(producto) {
 }
 
 
+function agregarAlCarrito(producto) {
+
+    const carrito = getCarrito();
+  
+    const productoEnCarrito = carrito.find(item => item.nombre === producto.nombre) || false;
+  
+    if (verduraEnCarrito) {
+      productoEnCarrito.cantidad++;
+    } else {
+      producto.cantidad = 1;
+      carrito.push(producto);
+    }
+    saveCarrito(carrito)
+  }
+
+
+function getCarrito () {
+    return JSON.parse(localStorage.getItem("carrito")) || [];
+  }
+function saveCarrito (carrito) {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  }
+
 /*function actualizarInputCantidadDeProductos() {
     let productos = localStorage.getItem("productos");
     
     let arrayProductos = [];
-
     if(productos !== null) {
         arrayProductos = productos.split(",");
     }
     inputCantidadDeProductos.value = arrayProductos.length;
 }
-
 function agregarALocalStorage (producto) {
-
     console.log(producto);
 }*/
 
